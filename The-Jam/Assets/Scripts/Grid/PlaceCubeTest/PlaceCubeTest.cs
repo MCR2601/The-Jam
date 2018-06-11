8177826e226b2d3bd5e41fde3100ea5fa8aafec9
Masaru26 <jonas.hirsch@gmx.at>
2018-06-11 20:38:44 +0200

    BasicGridFunctions and started with Map class for 3D Grid Block

---
+++ The-Jam/Assets/Scripts/Grid/PlaceCubeTest/PlaceCubeTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceCubeTest : MonoBehaviour {
+
+    private Grid grid;
+
+    private void Awake()
+    {
+        grid = FindObjectOfType<Grid>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hitInfo;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                PlaceCubeNear(hitInfo.point);
+            }
+        }
+    }
+
+    private void PlaceCubeNear(Vector3 nearPoint)
+    {
+        var finalPosition = grid.GetNearestPointOnGrid(nearPoint);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = finalPosition;
+    }
+}
